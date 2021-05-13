c498b7058d6cca80c86c3136ce25fc6f996d26cb
ikeyu0806 <ikeyu0806@gmail.com>
2021-05-13 09:50:26 +0900

    remove router, add auth condition

--- src/js/components/App.vue
+++ src/js/components/App.vue
@@ -1,17 +1,32 @@
 <template>
   <div>
-    <pocket-auth></pocket-auth>
-    <complete-auth></complete-auth>
+    <div v-if='authStatus == "requestTokenRequired"'><start-auth></start-auth></div>
+    <div v-else-if='authStatus == "accessTokenRequired"'><complete-auth></complete-auth></div>
+    <div v-else-if='authStatus == "authComplete"'><organize-pocket></organize-pocket></div>
   </div>
 </template>
 
 <script>
-import PocketAuth from './PocketAuth'
+import StartAuth from './StartAuth'
 import CompleteAuth from './CompleteAuth'
+import OrganizePocket from './OrganizePocket'
 
 export default {
-  components: { PocketAuth, CompleteAuth },
-  name: "App"
+  components: { StartAuth, CompleteAuth, OrganizePocket },
+  data: function() {
+          return {
+            authStatus: ""
+      }
+  },
+  mounted() {
+    if(localStorage.getItem('pocket_request_token') === null && localStorage.getItem('pocket_access_token') === null) {
+      this.authStatus = "requestTokenRequired"
+    } else if (localStorage.getItem('pocket_access_token') != null) {
+      this.authStatus = "authComplete"
+    } else if (localStorage.getItem('pocket_request_token') != null) {
+      this.authStatus = "accessTokenRequired"
+    }
+  }
 }
 
 </script>
--- src/js/routes.js
+++
@@ -1,7 +0,0 @@
-import App from './components/App'
-import PocketAuth from './components/PocketAuth'
-
-export default [
-  { path: '/', component: App },
-  { path: '/pocket_auth', component: PocketAuth },
-];
