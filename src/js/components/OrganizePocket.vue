d0735e2a7871df3759b9f79d57da7bead66b8ed4
ikeyu0806 <ikeyu0806@gmail.com>
2021-05-16 17:08:24 +0900

    add operation form view

--- src/js/components/OrganizePocket.vue
+++ src/js/components/OrganizePocket.vue
@@ -1,13 +1,35 @@
 <template>
   <section class="section">
     <div class="container">
+      <b-field label="表示記事数" :label-position="labelPosition">
+        <b-numberinput laceholder="10" :min="1" :max="100"></b-numberinput>
+      </b-field>
+      <b-field label="タグで検索" :label-position="labelPosition" grouped>
+        <b-taginput
+          :value="['レシピ', '映画']"
+          ellipsis
+          icon="label"
+          placeholder="タグを入力してください">
+        </b-taginput>
+      </b-field>
+      <b-field label="チェックされた記事にタグを付与" :label-position="labelPosition" grouped>
+        <b-taginput
+          :value="['レシピ', '映画']"
+          ellipsis
+          icon="label"
+          placeholder="タグを入力してください">
+        </b-taginput>
+      </b-field>
+      <b-button class="button is-primary">実行する</b-button>
       <b-table :data="articles"
                checkable
                :checkbox-position="checkboxPosition">
         <b-table-column field="resolved_title"
                         label="タイトル"
                         v-slot="props">
-          <a v-bind:href="props.row.given_url">{{ props.row.resolved_title }}</a>
+          <a v-bind:href="props.row.given_url"
+             target="_blank"
+             rel=“noopener”>{{ props.row.resolved_title }}</a>
         </b-table-column>
           <b-table-column field="time_added"
                   label="登録日時"
